540d2240fedd905d8ce8d77ab09eeb9809f24542
Aaron Schlesinger <aschlesinger@deis.com>
2016-05-17 16:10:02 -0700

    add fakes for the rc getter and lister

--- api/rc/getter.go
+++ api/rc/getter.go
@@ -10,3 +10,14 @@ import (
 type Getter interface {
 	Get(name string) (*api.ReplicationController, error)
 }
+
+// FakeGetter is a Getter implementation designed to be used in unit tests
+type FakeGetter struct {
+	RC  *api.ReplicationController
+	Err error
+}
+
+// Get is the Getter interface implementation. It simply returns f.RCList, f.Err
+func (f FakeGetter) Get(api.ListOptions) (*api.ReplicationController, error) {
+	return f.RC, f.Err
+}
--- api/rc/lister.go
+++ api/rc/lister.go
@@ -10,3 +10,14 @@ import (
 type Lister interface {
 	List(api.ListOptions) (*api.ReplicationControllerList, error)
 }
+
+// FakeLister is a Lister implementation designed to be used in unit tests
+type FakeLister struct {
+	RCList *api.ReplicationControllerList
+	Err    error
+}
+
+// List is the Lister interface implementation. It simply returns f.RCList, f.Err
+func (f FakeLister) List(api.ListOptions) (*api.ReplicationControllerList, error) {
+	return f.RCList, f.Err
+}
